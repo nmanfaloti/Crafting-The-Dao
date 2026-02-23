acd9c3bbd232580cce7929c903cf4f0f05d881f0
Lounol72 <subtil.louis2005@gmail.com>
2026-02-23 20:59:39 +0100

    Add authors to gradle.properties

--- src/main/java/net/CTD/CTDmod/items/misc/Elbaboss.java
+++ src/main/java/net/CTD/CTDmod/items/misc/Elbaboss.java
@@ -3,7 +3,7 @@ package net.CTD.CTDmod.items.misc;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.item.Item;
-import net.minecraft.world.item.ItemStack;
+
 import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.entity.player.Player;
 
