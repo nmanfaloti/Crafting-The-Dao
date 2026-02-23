25743b9a657c3657800fe22853f0c62d4b00452d
Lounol72 <subtil.louis2005@gmail.com>
2026-02-23 21:57:40 +0100

    File Added : SpiritStones.java

---
+++ src/main/java/net/CTD/CTDmod/block/misc/SpiritStones.java
@@ -0,0 +1,18 @@
+package net.CTD.CTDmod.block.misc;
+
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+
+/**
+ * SpiritStones is a rare gem findable in caves under the level 0
+ * Can be consumed for a cultivation XP boost.
+**/
+
+public class SpiritStones extends Block
+{
+  // Constructor
+  public SpiritStones(BlockBehaviour.Properties properties)
+  {
+    super(properties);
+  }
+}
